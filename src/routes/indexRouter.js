c46437e0422f69747d982c105f3e52bb11fe09eb
Captain-Fahd <omarfahd06@gmail.com>
2025-05-28 23:48:07 +1000

    Styled the main message board and the new Button. Simple styling was added to the main message board: - Hover transition on the messages - Simple navbar/header - Formatted the Dates to show simple DD/MM/YYYY - Added a background colour
    Added a button to redirect the user to the new message submission page.

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,14 +1,14 @@
 //Dummy Variables
 const messages = [
     {
-        text: "Hola",
+        text: "Hi",
         user: "Memon",
-        added: new Date()
+        added: new Date().toLocaleDateString('en-AU', { year: 'numeric', month: 'numeric', day: 'numeric'})
     },
     {
-        text: "I love F1",
+        text: "Hi",
         user: "AliBali",
-        added: new Date()
+        added: new Date().toLocaleDateString('en-AU', { year: 'numeric', month: 'numeric', day: 'numeric'})
     }
 ];
 const title = "Mini Message Board";
@@ -17,23 +17,27 @@ const title = "Mini Message Board";
 const { Router } = require('express');
 const indexRouter = Router();
 
+function formRedirect() {
+    console.log("Form Redirect");
+}
 
 
 //Main Route Paths
 indexRouter.get("/", (req, res) => {
-    res.render('index', {title: title, messages: messages});
+    res.render('index', {title: title, messages: messages, formRedirect: formRedirect});
 });
 
 indexRouter.get("/new", (req, res) => {
     res.render('form');
 });
 
+
 indexRouter.post("/new", (req, res) => {
    const {message, user} = req.body;
-   messages.push({
+   messages.unshift({
        text: message,
        user: user,
-       added: new Date()
+       added: new Date().toLocaleDateString('en-AU', { year: 'numeric', month: 'numeric', day: 'numeric'})
    });
    res.redirect('/');
 })
