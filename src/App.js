b2cb0d86e0dd5d71cbc094dbf2575dfc59ecd8c4
Captain-Fahd <omarfahd06@gmail.com>
2025-05-28 15:58:36 +1000

    Dummy Messages loaded The application now loads dummy messages and renders them on the main page.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-//App setup -- constants
+//App setup -- path/environment constants
 const express = require('express');
 const app = express();
 const PORT = 3001;
 const path = require('node:path');
 
+//App Routers
+const indexRouter= require('./routes/indexRouter');
+
 //App setup -- funcs
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
-app.get('/', (req, res) => {
-    res.render('index');
-})
+app.use('/', indexRouter);
 
 
-
+//App Listening
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 })
--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,5 +1,23 @@
+const messages = [
+    {
+        text: "Hola",
+        user: "Memon",
+        added: new Date()
+    },
+    {
+        text: "I love F1",
+        user: "AliBali",
+        added: new Date()
+    }
+];
+const title = "Mini Message Board";
+
 //Router Setup
 const { Router } = require('express');
 const indexRouter = Router();
 
-index.Router();
+indexRouter.get("/", (req, res) => {
+    res.render('index', {title: title, messages: messages});
+});
+
+module.exports = indexRouter;
